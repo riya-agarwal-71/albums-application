1a39a5e6317406003150b42452d7d9b48e59e858
Riya Agarwal <agarwal.riya71@gmail.com>
2022-01-20 02:55:43 +0530

    add sale and time

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,11 +8,18 @@ import {
 } from "@material-ui/core";
 
 class Item extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: Math.floor(Math.random() * 200 + 50),
+    };
+  }
+
   render() {
     return (
       <div>
         <Card style={{ display: "flex", height: "50px", padding: "0" }}>
-          <Box sx={{ display: "flex", flexDirection: "row" }}>
+          <Box sx={{ display: "flex", flexDirection: "row", width: "95%" }}>
             <div>
               <CardMedia
                 height='50px'
@@ -20,15 +27,37 @@ class Item extends Component {
                 image={this.props.itm.thumbnailUrl}
               />
             </div>
-            <div>
+            <div style={{ width: "100%" }}>
               <CardContent style={{ padding: "5px 0 1px 20px" }}>
-                <Typography component='div' variant='body1'>
-                  {this.props.itm.title}
-                </Typography>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <div>
+                    <Typography component='div' variant='body1'>
+                      {this.props.itm.title}
+                    </Typography>
+                  </div>
+                  {this.state.number < 75 ? (
+                    <div style={{ color: "red" }}>$ {this.state.number}</div>
+                  ) : (
+                    <div style={{ color: "green" }}>$ {this.state.number}</div>
+                  )}
+                </div>
                 <Typography component='div' variant='subtitle2'>
-                  <a href={this.props.itm.url} style={{ color: "gray" }}>
-                    {this.props.itm.url}
-                  </a>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <a href={this.props.itm.url} style={{ color: "gray" }}>
+                      {this.props.itm.url}
+                    </a>
+                    <div style={{ color: "gray" }}> 10:00 AM</div>
+                  </div>
                 </Typography>
               </CardContent>
             </div>
